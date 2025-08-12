b35cd471247add3e2b17fbeb99b234cd3ef06f5a
Badrul Hanif <hanifbaharuddin@gmail.com>
2025-08-12 21:52:18 +0800

    fix banner

--- app/child/page.tsx
+++ app/child/page.tsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 
 export default function ChildHomePage() {
   return (
-    <div className="flex flex-col gap-8 p-8">
-      <div className="flex flex-row px-6 py-8 gap-6 w-full items-center  rounded-2xl bg-zinc-900">
+    <div className="flex flex-col gap-8 p-4">
+      <div className="flex flex-row p-6 gap-6 w-full items-center  rounded-2xl bg-zinc-900">
         <Image
           src="/Images/brand-logo.png"
           alt="Brand Logo"
-          height={88}
-          width={88}
+          height={72}
+          width={72}
         />
         <div className="flex flex-col">
-          <h1 className="text-2xl font-medium">Der&apos; Cabin Bistro</h1>
+          <h1 className="text-2xl font-semibold">Der&apos; Cabin Bistro</h1>
           <p className="text-lg text-yellow-500">Comfort food, cozy moments</p>
         </div>
       </div>
