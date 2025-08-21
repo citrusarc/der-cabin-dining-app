56c963aeaf25e53303673818911fc7ba7a7ece77
Badrul Hanif <hanifbaharuddin@gmail.com>
2025-08-21 09:25:58 +0800

    new layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,25 @@
 export default function HomePage() {
   return (
-    <div
-      className="flex items-center justify-center h-screen w-screen bg-zinc-900 overflow-hidden"
-      style={{
-        backgroundImage: "url('/Images/bg-brand-logo.png')",
-        backgroundRepeat: "repeat",
-        backgroundSize: "120px auto",
-      }}
-    >
-      <div className="hidden md:block w-[460px] h-[880px]">
-        <iframe
-          src="child"
-          title="Der Cabin Dining App"
-          className="w-full h-full rounded-2xl"
-          allowFullScreen
-        />
+    <>
+      <div
+        className="hidden md:flex items-center justify-center h-screen w-screen bg-zinc-900 overflow-hidden"
+        style={{
+          backgroundImage: "url('/Images/bg-brand-logo.png')",
+          backgroundRepeat: "repeat",
+          backgroundSize: "120px auto",
+        }}
+      >
+        <div className="w-[460px] h-[880px]">
+          <iframe
+            src="child"
+            title="Der Cabin Dining App"
+            className="w-full h-full rounded-2xl"
+            allowFullScreen
+          />
+        </div>
       </div>
-      <div className="md:hidden w-full h-screen">
+
+      <div className="md:hidden w-full h-screen bg-zinc-900">
         <iframe
           src="child"
           title="Der Cabin Dining App"
@@ -24,6 +27,6 @@ export default function HomePage() {
           allowFullScreen
         />
       </div>
-    </div>
+    </>
   );
 }
