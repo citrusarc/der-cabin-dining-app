7a7d7c7fb0af256cb3439a8877e8a169a6b98d6a
Badrul Hanif <hanifbaharuddin@gmail.com>
2025-08-22 16:16:06 +0800

    fix iframe height

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div
-      className="flex items-center justify-center h-screen w-screen bg-zinc-900 overflow-hidden"
+      className="flex items-center justify-center w-screen h-[100dvh] bg-zinc-900 overflow-hidden"
       style={{
         backgroundImage: "url('/Images/bg-brand-logo.png')",
         backgroundRepeat: "repeat",
@@ -12,7 +12,7 @@ export default function HomePage() {
         <iframe
           src="child"
           title="Der Cabin Dining App"
-          className="w-full h-screen rounded-2xl"
+          className="w-full h-full rounded-2xl"
           allowFullScreen
         />
       </div>
